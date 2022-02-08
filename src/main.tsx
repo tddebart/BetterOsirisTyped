0f0702bf9bb710ce240a3e7944a16209c59e1442
tddebart <tddebart@gmail.com>
2022-02-08 21:34:38 +0100

    it's WORKING

--- src/main.tsx
+++ src/main.tsx
@@ -24,49 +24,92 @@ setTimeout(function () {
 
 }, 1000)
 
-window.onclick = function () {
+setTimeout(function () {
     changed()
+}, 3000)
+
+window.onclick = function () {
+    // changed()
+    setTimeout(function () {changed()}, 100)
 };
 
 function changed () {
+    console.log("changed called")
     let calendarList = document.getElementsByTagName("page-calendar");
     if(calendarList.length == 0 || document.getElementsByClassName("dayElement").length != 0) return;
     const calendar = calendarList[0];
     if(calendar == null) return;
+
+    clearPrevious(calendar);
+
+    var clickableDays = document.getElementsByTagName("osi-calendar-day");
+    for (let i = 0; i < clickableDays.length; i++) {
+        const clickableDay = clickableDays[i];
+        if(clickableDay.children.length == 0) continue;
+        if(clickableDay.children[0].children.length == 1) {
+            var clickObject = clickableDay.children[0].children[0] as HTMLElement;
+            if(clickableDay.getElementsByClassName("disabled").length == 0) {
+                clickObject.click();
+            }
+        }
+    }
+
     // @ts-ignore
     var dates = calendar.textContent.split("\n").map((i :string) => i.trim()).filter(function(entry : string) {
         return (entry.trim() != '' && entry != "expand_less");
     })
     dates.splice(0, 20)
     dates.pop()
-    let render : any[] = []
-    // for (let i = 0; i < dates.length; i++) {
-    //     let date = dates[i];
-    //     let iExtra = 0;
-    //     if(days.some(p => date.indexOf(p) > -1)) {
-    //         // console.log("found day: " + date);
-    //         // let dayElement = document.createElement("div");
-    //         // dayElement.className = "dayElement";
-    //         // dayElement.innerHTML = date;
-    //         // calendar.getElementsByClassName("fill-width fill-height")[0].appendChild(dayElement);
-    //         calendar.getElementsByClassName("fill-width fill-height")[0].className = "fill-width fill-height dayGrid";
-    //
-    //         iExtra++;
-    //         while(dates[i + iExtra] != null && days.every(p => dates[i+iExtra].indexOf(p) == -1)) {
-    //             // var dateElement = document.createElement("div");
-    //             // dateElement.className = "date";
-    //             // dateElement.innerHTML = dates[i + iExtra];
-    //             // dayElement.appendChild(dateElement);
-    //             iExtra++;
-    //         }
-    //         console.log("found day: " + dates[i]);
-    //         render.push(<DateElement dates={dates.slice(i, i+iExtra) }/>);
-    //         i += iExtra-1;
-    //     }
-    // }
-    render.push(<Calendar/>);
-    render.push(<Hours dates={[]}/>)
-    ReactDOM.render(render, calendar.getElementsByClassName("fill-width fill-height")[0]);
+    ReactDOM.render(<Calendar/>, calendar.getElementsByClassName("fill-width fill-height")[0]);
+    let parent = calendar.getElementsByClassName("rightHours")[0]
+
+    var dayIndex = 0;
+    for (let i = 0; i < dates.length; i++) {
+        let date = dates[i];
+        let iExtra = 0;
+        if(days.some(p => date.indexOf(p) > -1)) {
+            iExtra++;
+            while(dates[i + iExtra] != null && days.every(p => dates[i+iExtra].indexOf(p) == -1)) {
+                iExtra++;
+            }
+
+            console.log("found day: " + dates[i]);
+            for (let j = i; j < i+iExtra-1; j+=5) {
+                const startTime = dates[j+1]
+                if(isNaN(parseInt(startTime.slice(0,2)))) {
+                    return;
+                }
+                const endTime = dates[j+2]
+                const name = dates[j+3]
+                const docent = dates[j+4]
+                const room = dates[j+5]
+
+                // if(i == 0) {
+                    var d1 = new Date();
+                    d1.setHours(parseInt(startTime.slice(0,2)));
+                    var mins1 = isNaN(parseInt(startTime.slice(2,5))) ? parseInt(startTime.slice(3,5)) : parseInt(startTime.slice(2,5));
+                    d1.setMinutes(mins1);
+                    var d2 = new Date();
+                    d2.setHours(parseInt(endTime.slice(0,2)));
+                    var mins2 = isNaN(parseInt(endTime.slice(2,5))) ? parseInt(endTime.slice(3,5)) : parseInt(endTime.slice(2,5));
+                    d2.setMinutes(mins2);
+                    var diffHour = d2.getHours() - d1.getHours();
+                    var diffMin = d2.getMinutes() - d1.getMinutes();
+                    var part = document.createElement("div")
+                    part.className = "part"
+                    part.style.top =  d1.getMinutes()*2.5 + "px";
+                    part.style.height = (diffHour * 75*2) + (diffMin * 2.5) + "px"
+
+
+
+                    parent.children[dayIndex].children[(d1.getHours()-9)*2+1].appendChild(part)
+
+                // }
+            }
+            dayIndex++;
+            i += iExtra-1;
+        }
+    }
 }
 
 const Calendar = () => {
@@ -89,10 +132,10 @@ const Calendar = () => {
                 </div>
                 <div className="rightHours">
                     {
-                        days.map(day =>
+                        days.map(() =>
                             <div className={"dayContainer"}>
                                 {
-                                    hours.map(hour =>
+                                    hours.map(() =>
                                         <div className="hourContainer"/>
                                     )
                                 }
@@ -100,18 +143,20 @@ const Calendar = () => {
                         )
                     }
                 </div>
-                {/*{days.map(day => <div key={"bottom"+day} className="bottomCalendar">{}</div>)}*/}
             </div>
         </>
     )
 }
 
-const Hours = (props : {dates : string[]}) => {
-    return (
-        <>
-            <div className={"frontHourHolder"}>
-
-            </div>
-        </>
-    )
+function clearPrevious(calendar: Element) {
+    let parent = calendar.getElementsByClassName("rightHours")[0]
+    if(parent != null) {
+        for (let i = 0; i < parent.children.length; i++) {
+            let child = parent.children[i]
+            for (let j = 0; j < child.children.length; j++) {
+                let child2 = child.children[j]
+                child2.innerHTML = ""
+            }
+        }
+    }
 }
